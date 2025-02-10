be8e1de2fc66ac3290a772d901ddead5d0033650
SHANTO612 <fahadshanto653@gmail.com>
2025-02-10 15:39:50 +0600

    feat: add signup page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/Header/Header";
 import NotFound from "./components/NotFound/NotFound";
 import Signin from "./components/signin/signin";
+import Signup from "./components/signup/signup";
 function App() {
   return (
     <>
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" />
           <Route path="/signin" element={<Signin/>} />
+          <Route path="/signup" element={<Signup/>} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
--- frontend/src/components/signup/signup.tsx
+++ frontend/src/components/signup/signup.tsx
@@ -2,9 +2,15 @@ import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
-import signInIcon from "../../../public/signin.avif";
+import signUpIcon from "../../../public/sign-up.avif";
 import { Link } from "react-router-dom";
 const contactSchema = z.object({
+  firstName: z
+    .string()
+    .min(2, { message: "firstName must be at least 2 characters" }),
+  lastName: z
+    .string()
+    .min(2, { message: "lastName must be at least 2 characters" }),
   email: z.string().email({ message: "Invalid email address" }),
   password: z
     .string()
@@ -13,7 +19,7 @@ const contactSchema = z.object({
 
 type ContactValues = z.infer<typeof contactSchema>;
 
-function Signin() {
+function SignupPage() {
   const [isLoading, setIsLoading] = useState(false);
 
   const {
@@ -31,14 +37,66 @@ function Signin() {
   };
 
   return (
-    <section className="min-h-screen bg-gray-100  dark:bg-gray-500 flex flex-col items-center justify-center p-4">
+    <div className="min-h-screen dark:bg-gray-500 flex flex-col items-center justify-center p-4">
       {/* Main Container */}
       <div className="w-full max-w-4xl p-8 bg-[#0A1124] rounded-lg shadow-xl">
         <div className="grid md:grid-cols-2 gap-8">
           {/* Left Section */}
+          <div className="flex flex-col justify-between">
+            <div>
+              <h2 className="text-2xl font-bold text-white mb-8">Welcome</h2>
+            </div>
+            <div className="flex-grow flex items-center justify-center">
+              <img
+                src={signUpIcon || "/placeholder.png"}
+                alt="SignUp"
+                className="w-full h-auto max-h-[400px] object-contain"
+              />
+            </div>
+          </div>
+
+          {/* Right Section */}
           <div>
             <h2 className="text-2xl font-bold text-white mb-8">Get in Touch</h2>
             <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+              <div>
+                <label
+                  htmlFor="firstName"
+                  className="block text-start text-sm font-medium text-gray-200"
+                >
+                  First Name
+                </label>
+                <input
+                  {...register("firstName")}
+                  type="text"
+                  className="mt-1 block w-full px-3 py-2 bg-gray-700 border border-gray-600 rounded-md text-white placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+                  placeholder="Enter full name"
+                />
+                {errors.firstName && (
+                  <p className="mt-1 text-sm text-red-500">
+                    {errors.firstName.message}
+                  </p>
+                )}
+              </div>
+              <div>
+                <label
+                  htmlFor="lastName"
+                  className="block text-start text-sm font-medium text-gray-200"
+                >
+                  Username
+                </label>
+                <input
+                  {...register("lastName")}
+                  type="text"
+                  className="mt-1 block w-full px-3 py-2 bg-gray-700 border border-gray-600 rounded-md text-white placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+                  placeholder="Enter username"
+                />
+                {errors.lastName && (
+                  <p className="mt-1 text-sm text-red-500">
+                    {errors.lastName.message}
+                  </p>
+                )}
+              </div>
               <div>
                 <label
                   htmlFor="email"
@@ -84,41 +142,27 @@ function Signin() {
               <button
                 type="submit"
                 disabled={isLoading}
-                className="w-full bg-blue-500 text-white py-2 px-4 rounded-md
-                 hover:bg-blue-600 transition-colors disabled:opacity-50"
+                className="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 transition-colors disabled:opacity-50"
               >
-                {isLoading ? "Sending..." : "Sign in"}
+                {isLoading ? "Sending..." : "Sign Up"}
               </button>
               <div className="text-center">
                 <span className="text-sm text-white">
-                  Don't have an account?{" "}
+                  Already have an account?{" "}
                 </span>
                 <Link
-                  to="/signup"
+                  to="/signin"
                   className="text-blue-500 hover:underline text-sm"
                 >
-                  Sign Up
+                  Sign In
                 </Link>
               </div>
             </form>
           </div>
-          {/* Right Section */}
-          <div className="flex flex-col justify-between">
-            <div>
-              <h2 className="text-2xl font-bold text-white mb-8">Welcome</h2>
-            </div>
-            <div className="flex-grow flex items-center justify-center">
-              <img
-                src={signInIcon || "/placeholder.png"}
-                alt="SignUp"
-                className="w-full h-auto max-h-[400px] object-contain"
-              />
-            </div>
-          </div>
         </div>
       </div>
-    </section>
+    </div>
   );
 }
 
-export default Signin;
+export default SignupPage;
