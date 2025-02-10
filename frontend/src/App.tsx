76d0c7bc31f1502b07f6b30a191ca8b402c51bbc
SHANTO612 <fahadshanto653@gmail.com>
2025-02-10 15:34:21 +0600

    feat: add signin page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/Header/Header";
 import NotFound from "./components/NotFound/NotFound";
+import Signin from "./components/signin/signin";
 function App() {
   return (
     <>
@@ -8,6 +9,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" />
+          <Route path="/signin" element={<Signin/>} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
