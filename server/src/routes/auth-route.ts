ca50a006a30737f22c1d7f903074e52f6229db52
Ajor-Saha <sokarama79@gmail.com>
2025-02-28 22:47:58 +0600

    feat:add auth system in backend

--- server/src/routes/auth-route.ts
+++ server/src/routes/auth-route.ts
@@ -1,10 +1,15 @@
 import { Router } from 'express'
-import { signup } from '../controllers/auth-controllers'
+import { login, logout, signup, verifyEmail } from '../controllers/auth-controllers'
+import { verifyJWT } from '../middleware/auth-middleware';
 
 
 const router = Router();
 
-router.route('/add-new-user').post(signup);
+
+router.route('/signup').post(signup);
+router.route("/signin").post(login);
+router.route("/signout").post(verifyJWT, logout);
+router.route('/email-verification').post(verifyEmail);
 
 
-export default router;
+export default router; 
