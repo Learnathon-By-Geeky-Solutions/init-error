3e2adaddc03f0c8d24efc2ba2a137f61f6f7b855
a-z-nath <at.avijit.deb@gmail.com>
2025-03-08 00:58:11 +0600

    fix: refactor OTP verification component and add validation schema

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,10 +4,10 @@ import HomePage from "./pages/Home";
 import Footer from "./components/Footer/Footer";
 import Dashboard from "./pages/DashBoard";
 import UserAccount from "./pages/UserAccount";
-import Header from "./components/header/Header";
+import Header from "./components/Header/Header";
 import SignInPage from "./pages/SignIn";
 import SignupPage from "./pages/SignUp";
-import { OTPVerification } from "./pages/email_verification";
+import OTPVerification from "./pages/email_verification";
 
 function App() {
   return (
@@ -18,7 +18,7 @@ function App() {
         <Route path="/signin" element={<SignInPage />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/verify/:email" element={<OTPVerification/>} />
+        <Route path="/verify/:email" element={<OTPVerification />} />
         <Route path="/account/profile" element={<UserAccount />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
--- frontend/src/pages/email_verification.tsx
+++ frontend/src/pages/email_verification.tsx
@@ -1,196 +1,132 @@
-import type React from "react";
+import { REGEXP_ONLY_DIGITS } from "input-otp";
 
-import { useState, useRef, useEffect } from "react";
 import {
-  Hexagon,
-  ArrowRight,
-  Loader2,
-  CheckCircle,
-  AlertCircle,
-} from "lucide-react";
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSlot,
+} from "@/components/ui/input-otp";
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
+// import Image from "next/image";
+import { useParams, useNavigate } from "react-router-dom";
+import { toast } from "sonner";
+import { useForm } from "react-hook-form";
+import { verifySchema } from "@/schemas/authschema";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { ApiResponse } from "@/types/api-success-type";
+import { AxiosError } from "axios";
+import { useState } from "react";
 import {
   Card,
   CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { Alert, AlertDescription } from "@/components/ui/alert";
+import { Hexagon } from "lucide-react";
+import { Axios } from "@/config/axios";
 
-const OTP_LENGTH = 6;
+const OTPVerification = () => {
+  const navigate = useNavigate();
+  const params = useParams<{ email: string }>();
 
-export function OTPVerification() {
-  const [otp, setOtp] = useState<string[]>(new Array(OTP_LENGTH).fill(""));
-  const [activeInput, setActiveInput] = useState<number>(0);
-  const [isVerifying, setIsVerifying] = useState<boolean>(false);
-  const [resendCountdown, setResendCountdown] = useState<number>(0);
-  const [notification, setNotification] = useState<{
-    type: "success" | "error" | null;
-    message: string;
-  }>({
-    type: null,
-    message: "",
+  const form = useForm<z.infer<typeof verifySchema>>({
+    resolver: zodResolver(verifySchema),
+    mode: "onChange",
+    defaultValues: {
+      email: params.email,
+      code: "",
+    },
   });
-  const inputRefs = useRef<(HTMLInputElement | null)[]>([]);
 
-  useEffect(() => {
-    inputRefs.current[0]?.focus();
-  }, []);
+  const [otpValue, setOtpValue] = useState("");
+  const [loading, setLoading] = useState<boolean>(false);
 
-  useEffect(() => {
-    if (resendCountdown > 0) {
-      const timer = setTimeout(
-        () => setResendCountdown(resendCountdown - 1),
-        1000
-      );
-      return () => clearTimeout(timer);
-    }
-  }, [resendCountdown]);
-
-  const handleChange = (index: number, value: string) => {
-    const newOtp = [...otp];
-    newOtp[index] = value.slice(0, 1);
-    setOtp(newOtp);
-
-    if (value && index < OTP_LENGTH - 1) {
-      setActiveInput(index + 1);
-      inputRefs.current[index + 1]?.focus();
-    }
+  const handleOtpChange = (value: string) => {
+    setOtpValue(value);
+    form.setValue("code", value);
   };
 
-  const handleKeyDown = (
-    index: number,
-    e: React.KeyboardEvent<HTMLInputElement>
-  ) => {
-    if (e.key === "Backspace" && !otp[index] && index > 0) {
-      setActiveInput(index - 1);
-      inputRefs.current[index - 1]?.focus();
-    }
-  };
-
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    setIsVerifying(true);
-    const enteredOtp = otp.join("");
-
-    // Simulate API call
-    await new Promise((resolve) => setTimeout(resolve, 1500));
-
-    if (enteredOtp === "123456") {
-      // Example correct OTP
-      setNotification({
-        type: "success",
-        message: "Verification successful! Redirecting...",
+  const onSubmit = async (data: z.infer<typeof verifySchema>) => {
+    setLoading(true);
+    try {
+      const response = await Axios.post(`/api/auth/email-verification`, {
+        email: params.email,
+        code: data.code,
       });
-    } else {
-      setNotification({
-        type: "error",
-        message: "Invalid OTP. Please try again.",
-      });
-    }
-    setIsVerifying(false);
-  };
 
-  const handleResend = () => {
-    setOtp(new Array(OTP_LENGTH).fill(""));
-    setActiveInput(0);
-    setResendCountdown(30);
-    setNotification({
-      type: "success",
-      message: "A new OTP has been sent to your email.",
-    });
+      toast.success(response.data.message || "Account verified successfully.");
+
+      navigate("/signin");
+    } catch (error) {
+      console.error("Error during sign-up:", error);
+      const axiosError = error as AxiosError<ApiResponse>;
+      const errorMessage =
+        axiosError.response?.data.message ??
+        "There was a problem with your sign-up. Please try again.";
+      toast.error(errorMessage);
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
-    <div className="min-h-screen py-32">
-    <Card className="w-full max-w-md mx-auto dark:bg-gray-800 dark:text-white">
-      <CardHeader>
-        <div className="flex justify-center mb-4">
-          <div className="bg-primary p-3 rounded-xl dark:bg-primary-dark">
-            <Hexagon className="w-6 h-6 text-primary-foreground dark:text-primary-dark-foreground" />
-          </div>
-        </div>
-        <CardTitle className="text-2xl font-bold text-center dark:text-white">
-          Verify Your Account
-        </CardTitle>
-        <CardDescription className="text-center dark:text-gray-300">
-          Enter the 6-digit code sent to your email
-        </CardDescription>
-      </CardHeader>
-      <CardContent>
-        <form onSubmit={handleSubmit}>
-          <div className="flex justify-center gap-2 mb-6">
-            {otp.map((digit, index) => (
-              <Input
-                key={index}
-                ref={(el) => (inputRefs.current[index] = el)}
-                type="text"
-                inputMode="numeric"
-                maxLength={1}
-                value={digit}
-                onChange={(e) => handleChange(index, e.target.value)}
-                onKeyDown={(e) => handleKeyDown(index, e)}
-                className={`w-12 h-14 text-center text-2xl font-semibold ${
-                  activeInput === index
-                    ? "border-primary"
-                    : "dark:border-primary-dark"
-                } focus:border-primary focus:ring-2 focus:ring-primary focus:ring-opacity-50 transition-all duration-300 dark:bg-gray-700 dark:text-white`}
-              />
-            ))}
-          </div>
-          {notification.type && (
-            <Alert
-              variant={
-                notification.type === "success" ? "default" : "destructive"
-              }
-              className={`mb-4 ${
-                notification.type === "success"
-                  ? "bg-green-100 text-green-800 border-green-300 dark:bg-green-800 dark:text-green-200 dark:border-green-700"
-                  : ""
-              }`}
-            >
-              {notification.type === "success" ? (
-                <CheckCircle className="h-4 w-4" />
-              ) : (
-                <AlertCircle className="h-4 w-4" />
-              )}
-              <AlertDescription>{notification.message}</AlertDescription>
-            </Alert>
-          )}
-          <Button
-            type="submit"
-            className="w-full dark:bg-slate-600 dark:text-white dark:hover:bg-green-800"
-            disabled={isVerifying || otp.some((digit) => !digit)}
-          >
-            {isVerifying ? (
-              <>
-                <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-                Verifying
-              </>
-            ) : (
-              <>
-                Verify <ArrowRight className="ml-2 w-5 h-5" />
-              </>
-            )}
-          </Button>
-        </form>
-      </CardContent>
-      <CardFooter className="flex justify-center">
-        <Button
-          variant="link"
-          onClick={handleResend}
-          disabled={resendCountdown > 0}
-          className="dark:text-white"
-        >
-          {resendCountdown > 0
-            ? `Resend in ${resendCountdown}s`
-            : "Resend Code"}
-        </Button>
-      </CardFooter>
-    </Card>
+    <div className="flex justify-center pt-52 min-h-screen">
+      <div className="flex flex-col justify-center items-center h-full ">
+        <Card className="w-full max-w-md">
+          <CardHeader>
+            <div className="flex justify-center mb-4">
+              <div className="bg-primary p-3 rounded-xl dark:bg-primary-dark">
+                <Hexagon className="w-6 h-6 text-primary-foreground dark:text-primary-dark-foreground" />
+              </div>
+            </div>
+            <CardTitle className="text-2xl font-bold text-center dark:text-white">
+              Verify Your Account
+            </CardTitle>
+            <CardDescription className="text-center dark:text-gray-300">
+              Enter the 6-digit code sent to your email
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+              <div className="flex flex-col justify-center items-center">
+                <label htmlFor="code" className="block font-semibold text-lg">
+                  Enter OTP
+                </label>
+                <InputOTP
+                  maxLength={6}
+                  pattern={REGEXP_ONLY_DIGITS}
+                  value={otpValue}
+                  onChange={handleOtpChange}
+                >
+                  <InputOTPGroup>
+                    {[...Array(6)].map((_, index) => (
+                      <InputOTPSlot key={index} index={index} />
+                    ))}
+                  </InputOTPGroup>
+                </InputOTP>
+                {form.formState.errors.code && (
+                  <p className="text-red-600">
+                    {form.formState.errors.code.message}
+                  </p>
+                )}
+              </div>
+              <div className="flex justify-center">
+                <Button
+                  type="submit"
+                  disabled={loading}
+                  variant="default"
+                  className="px-2"
+                >
+                  {loading ? "Verifying..." : "Verify OTP"}
+                </Button>
+              </div>
+            </form>
+          </CardContent>
+        </Card>
+      </div>
     </div>
   );
-}
+};
+
+export default OTPVerification;
